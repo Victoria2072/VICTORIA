60fdb75e5d84b10b381078f93b2c0e6f58a5f524
Victoria2072 <144427057+Victoria2072@users.noreply.github.com>
2023-09-17 15:07:03 +0900

    Add files via upload

---
+++ assigment.py
@@ -0,0 +1,17 @@
+#assignment 1
+hours=int(input("enter hours"))
+rate=float(input("enter rate"))
+salary=hours*rate
+print("salary:", salary)
+
+#assignment 2
+celsius=int(input("enter celsius temperaturs"))
+fahrenheit=celsius*9/5+32
+print("fahrenteit temperature:" , fahrenheit)
+
+#assigment
+seconds=int(input("enter celsius temperature"))
+hours=seconds// 3600
+minutes=(seconds%3600) // 60
+seconds2=seconds%60
+print(seconds, "seconds is:" ,hours, "hours" ,minutes, "minutes" ,seconds2, "seconds")
